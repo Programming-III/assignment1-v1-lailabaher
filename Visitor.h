06ceda15d44eb6e262907e44833263fabfeca8b4
lailabaher <lailabaher07@gmail.com>
2025-11-12 15:20:24 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -14,6 +14,10 @@ class visitor
         visitor (string visitorName, int ticketsBought);
         void displayInfo();
         ~visitor();
+        string getVisitorName();
+        void setVisitorName(string v);
+        int getTicketsBought();
+        void setTicketsBought(int t);
 };
 
 #endif // VISITOR_H
